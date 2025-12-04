69e442298e3604a0d5b950922bcb38b6f9f7a622
EmannuelMt <seu-email@exemplo.com>
2025-12-04 18:25:46 -0300

    fix: corrige extensão da imagem ecos da realidade

--- src/data/projectsData.js
+++ src/data/projectsData.js
@@ -2,7 +2,7 @@ import buguerDelicia from '../assets/images/buguer-delicia.jpg';
 import transitaAI from '../assets/images/transita-ai.jpg';
 import grupoAlmeida from '../assets/images/grupo-almeida-capital.png';
 import emannuelDev from '../assets/images/emannuel-dev.jpg';
-import ecosdarealidade from '../assets/images/ecos-da-realidade.png';
+import ecosdarealidade from '../assets/images/ecos-da-realidade.jpg';
 export const servicesData = [
   // Serviços Principais
   {
